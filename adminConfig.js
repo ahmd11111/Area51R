78891262acfaeeabf3f7e89c717a50ecd8d42d34
ahmd11111 <ahmdgaming2011@gmail.com>
2026-04-18 20:14:54 +0300

    Add files via upload

---
+++ adminConfig.js
@@ -0,0 +1,7 @@
+// Admin Configuration
+module.exports = {
+  adminEmail: 'X5Admin@support.com',
+  adminPassword: 'X5Admin2026',
+  siteName: 'X5 Store',
+  siteDescription: 'Your premium shopping destination'
+};
