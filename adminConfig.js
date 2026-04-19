802bf43f7415037fc33809e07021adbf92095ed8
ahmd11111 <ahmdgaming2011@gmail.com>
2026-04-19 03:09:05 +0300

    Delete adminConfig.js

--- adminConfig.js
+++
@@ -1,7 +0,0 @@
-// Admin Configuration
-module.exports = {
-  adminEmail: 'X5Admin@support.com',
-  adminPassword: 'X5Admin2026',
-  siteName: 'X5 Store',
-  siteDescription: 'Your premium shopping destination'
-};
