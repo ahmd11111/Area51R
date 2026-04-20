5d8bc2817cd213ffcca96c7741a6cc92c92a7700
ahmd11111 <ahmdgaming2011@gmail.com>
2026-04-20 04:27:04 +0300

    Delete adminConfig.js

--- adminConfig.js
+++
@@ -1,7 +0,0 @@
-// Admin Configuration
-module.exports = {
-  adminEmail: 'mohamedalkhameiri15@gmail.com',
-  adminPassword: 'Moza3119989',
-  siteName: 'Area Store',
-  siteDescription: 'Your premium shopping destination'
-};
